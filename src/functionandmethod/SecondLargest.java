78ad610d7b79067312d01634c8c6c9faf8bd3c6e
shubhamjha831 <shubhamjhamailme@gmail.com>
2026-08-20 23:57:36 +0530

    SecondLargest

---
+++ src/functionandmethod/SecondLargest.java
@@ -0,0 +1,49 @@
+package functionandmethod;
+
+import java.util.*;
+
+class SecondLargest {
+
+    static int findSecondLargest(int[] arr) {
+
+        int largest = Integer.MIN_VALUE;
+        int secondLargest = Integer.MIN_VALUE;
+
+        for (int num : arr) {
+
+            if (num > largest) {
+                secondLargest = largest;
+                largest = num;
+            }
+            else if (num > secondLargest && num != largest) {
+                secondLargest = num;
+            }
+        }
+
+        return secondLargest;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter size of array: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter " + n + " elements:");
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int result = findSecondLargest(arr);
+
+        if (result == Integer.MIN_VALUE) {
+            System.out.println("Second largest element does not exist");
+        } else {
+            System.out.println("Second largest = " + result);
+        }
+    }
+}
