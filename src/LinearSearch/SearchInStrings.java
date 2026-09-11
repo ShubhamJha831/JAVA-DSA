7744ed5904ffe53150c412a86074844b67993e62
shubhamjha831 <shubhamjhamailme@gmail.com>
2026-09-11 18:58:12 +0530

    Search in string

--- src/LinearSearch/SearchInStrings.java
+++ src/LinearSearch/SearchInStrings.java
@@ -1,7 +1,20 @@
 package LinearSearch;
 
 public class SearchInStrings {
-    public static void main() {
-
+    public static void main(String[] args) {
+    String name = "Shubham Jha";
+    char target = 'l';
+    System.out.println(search(name, target));
+    }
+    public static boolean search(String str , char target){
+        if  (str.length() == 0){
+            return false;
+        }
+        for(int i = 0; i < str.length(); i++){
+            if(str.charAt(i) == target){
+                return true;
+            }
+        }
+        return false;
     }
 }
